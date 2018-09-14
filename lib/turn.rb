b28aca91eba73d1e2937462e2dd922911d9a21c8
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-09-14 15:34:03 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,5 +30,9 @@ def turn(board)
     display_board(board)
   else
     puts "Please enter 1-9:"
+    index = input_to_index(gets.strip)
+    if valid_move?(board, index)
+      move(board, index)
+      display_board(board)
   end
 end
